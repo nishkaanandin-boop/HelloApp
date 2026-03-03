2ce4452fae5e47c8005a1e3db4ab73600055ceea
Nishka Anand <nishka.anand.in@gmail.com>
2026-03-03 09:21:07 +0530

    UC2_OOPSBannerApp

--- UC1_PrintOOPS.java
+++ UC1_PrintOOPS.java
@@ -1,5 +1,11 @@
-public class UC1_PrintOOPS {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
-}
+javac "$(FULL_CURRENT_PATH)" && java "$(NAME_PART)"
+//Example for OS which stands for Operating System
+System.out.println("   ***   " + " " + "   ***** ");
+System.out.println(" **   ** " + " " + " **      ");
+System.out.println("**       " + " " + "**       ");
+System.out.println("**       " + " " + "  *****  ");
+System.out.println("**       " + " " + "       **");
+System.out.println(" **   ** " + " " + " **    **");
+System.out.println("   ***   " + " " + "  ****** ");
+public Class OOPSBannerApp {
+public state void main(String[] args}
